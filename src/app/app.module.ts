31e442d50e0c0f184be5972fe892cb6d9489ac42
Boskin Erkocevic <boer8364@colorado.edu>
2018-03-10 13:02:20 -0700

    Added declarations for the voice detection page. I don't know if this will work...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {VoiceDetectionPage } from '../pages/voice-detection/voice-detection';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    VoiceDetectionPage 
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    VoiceDetectionPage
   ],
   providers: [
     StatusBar,
